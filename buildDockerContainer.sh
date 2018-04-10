a0112d75ab74c4f73a563a9ddc7eec78ccc72234
luca vecchi <lucailvec@cln-237-157.eduroam.unimi.it>
2018-04-10 18:48:11 +0200

    errata in the buildContainer

--- buildDockerContainer.sh
+++ buildDockerContainer.sh
@@ -7,7 +7,7 @@ DB_PSW="ciao"
 #Look at the problem that this build will have, sometimes it required https://www.projectatomic.io/blog/2015/06/using-volumes-with-docker-can-cause-problems-with-selinux/
 docker run --rm --name oracle-xe \
 --detach \
--e ORACLE_PWD=ciao \
+-e ORACLE_PWD=$DB_PSW \
 -p 1521:1521 -p 8080:8080 \
 -v "$PWD/static_file":/u01/app/oracle/oradata \
 --shm-size 1GB oracle/database:11.2.0.2-xe-rlwrap
